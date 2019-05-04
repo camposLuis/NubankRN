6e3a1d1a44a6815ecb50df11d0f9243d5d6610bf
camposLuis <luis.campos.henrique@hotmail.com>
2019-05-04 01:22:34 -0300

    Criando Animações parte 2

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -1,13 +1,14 @@
-import { StyleSheet } from 'react-native';
+import { Animated, StyleSheet } from 'react-native';
 import styled from 'styled-components/native';
 
-export const Container = styled.ScrollView`
+export const Container = styled(Animated.ScrollView)`
   margin: 0 30px;
 `;
 
 export const Code = styled.View`
   padding: 10px;
   align-self: center;
+  overflow: hidden;
 `;
 
 export const Nav = styled.View`
